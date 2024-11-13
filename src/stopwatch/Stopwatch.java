ea29ede02af56c64cc4ff3c508a4d097dd496446
unknown <staralbum2@gmail.com>
2024-11-13 13:45:33 +0900

    add method

--- src/stopwatch/Stopwatch.java
+++ src/stopwatch/Stopwatch.java
@@ -8,49 +8,56 @@ import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class Stopwatch {
-	private Stopwatch(){
-		
+	private Stopwatch() {
+
 	}
+
 	private static Stopwatch insStopwatch = new Stopwatch();
+
 	public static Stopwatch getInstance() {
 		return insStopwatch;
 	}
-	
-private static Scanner scanner = new Scanner(System.in);
-	
+
+	private static Scanner scanner = new Scanner(System.in);
+
 	private static SimpleDateFormat sdf;
 	private static boolean isRun = true;
 	private static int minute;
-	private static int second;
-	
+
 	private StringBuffer bf = new StringBuffer();
 	private BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-	private BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out))
+	private BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
 	
-			public void run() {
-		
-		while(isRun) {
-			setStopwatch();
-			printMenu();
+	public void run() {
+		setStopwatch();
+		printMenu();
+		while (isRun) {
 			runStopwatch();
 		}
 	}
-	
+
 	private void setStopwatch() {
-	
+
 	}
+
 	private void printMenu() {
-		
+
 	}
+
 	private void runStopwatch() {
+		// s h 를 제외한 아무키를 누르면 재시작
+		boolean run = true;
 		
 	}
-	
+
 	private int inputNum(String msg) {
-		
-		bf.append(msg + " : ");
+		try {
+			wr.
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		
 		
 	}
-	
+
 }
